e740cca863708cfa36cfdc039c9dcc8672414fa2
Amadeus Justinn Haryanto <amadeusjustinn@gmail.com>
2024-07-26 10:06:39 -0500

    SvelteKit rewrite

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,17 @@
-import { expect, test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-test('home page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	await expect(page.locator('h1')).toBeVisible();
+const authFile = './playwright/.auth/user.json';
+
+test("authenticate", async ({ page }) => {
+	await page.goto("https://easy-speak.org/portal.php");
+	await expect(page.getByText(/The easy-Speak software allows/)).toBeVisible();
+	await page.locator("input#username").fill("username");
+	await page.locator('input[name="password"]').fill("password");
+	await page.getByRole("button", { name: "Log in" }).click();
+
+	await page.waitForURL("https://easy-speak.org/portal.php?marknow=0");
+	await expect(page.getByText(/Since 1924/)).toBeVisible();
+
+	// Save auth details in auth file
+	await page.context().storageState({ path: authFile });
 });
